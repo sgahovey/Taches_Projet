0cdcda6d59947a0e9349c77fd1fd618d9b86334a
AHOVEY Saint-George <sgahovey@gmail.com>
2025-05-15 10:50:10 +0400

    Initialisation du projet

---
+++ src/main/java/com/dreams/hellowordspring/taches/TachesApplication.java
@@ -0,0 +1,13 @@
+package com.dreams.hellowordspring.taches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TachesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TachesApplication.class, args);
+    }
+
+}
