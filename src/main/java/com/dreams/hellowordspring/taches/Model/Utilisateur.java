5dadb550284d47df867e7f2bd4a375c4cce1d833
AHOVEY Saint-George <sgahovey@gmail.com>
2025-05-15 11:00:41 +0400

    Création de l'entité Utilisateur

--- src/main/java/com/dreams/hellowordspring/taches/Model/Utilisateur.java
+++ src/main/java/com/dreams/hellowordspring/taches/Model/Utilisateur.java
@@ -1,4 +1,9 @@
 package com.dreams.hellowordspring.taches.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "utilisateur")
 public class Utilisateur {
-}
+  }
