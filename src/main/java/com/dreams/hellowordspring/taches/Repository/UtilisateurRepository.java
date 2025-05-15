d640cfb9220feeb0f129dacb6697e83cf9463359
AHOVEY Saint-George <sgahovey@gmail.com>
2025-05-15 12:48:03 +0400

    Modification du Repository pour Utilisateur et ajout du CRUD

--- src/main/java/com/dreams/hellowordspring/taches/Repository/UtilisateurRepository.java
+++ src/main/java/com/dreams/hellowordspring/taches/Repository/UtilisateurRepository.java
@@ -2,9 +2,10 @@
 
     import com.dreams.hellowordspring.taches.Model.Utilisateur;
     import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.repository.CrudRepository;
     import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
     @RepositoryRestResource(path = "utilisateur")
 
-    public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+    public interface UtilisateurRepository extends CrudRepository<Utilisateur, Long> {
     }
