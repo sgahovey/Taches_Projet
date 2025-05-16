5dd1aa249818409775215d2de08a8fcb695e510d
AHOVEY Saint-George <sgahovey@gmail.com>
2025-05-16 09:06:11 +0400

    Création du Controller et Repository pour TacheSimple

--- src/main/java/com/dreams/hellowordspring/taches/Repository/TacheSimpleRepository.java
+++ src/main/java/com/dreams/hellowordspring/taches/Repository/TacheSimpleRepository.java
@@ -4,6 +4,11 @@ import com.dreams.hellowordspring.taches.Model.TacheSimple;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "tachesimple")
 public interface TacheSimpleRepository extends CrudRepository<TacheSimple, Long> {
+    List<TacheSimple> findByEstCompleteTrue();
+
+    List<TacheSimple> findByEstCompleteFalse();
 }
